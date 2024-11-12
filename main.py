ed33a7365c6c3c8ac1c31258d6ec7c8a2d6d9a5d
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2024-11-12 21:19:31 +0100

    access data from json file

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
+import json
 import os
 
-print(os.getcwd())
+def load_data(file_path):
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+animals_data = load_data('animals_data.json')
+
+
+for item in animals_data:
+    name = item.get("name")
+    characteristics = item.get("characteristics", {})
+    location = item["locations"][0]
+    diet = characteristics.get("diet")
+    animal_type = characteristics.get("type", "N/A")
+
+    if animal_type == "N/A":
+        print(f"Name: {name}\nDiet: {diet}\nlocation: {location}\n")
+    else:
+        print(f"Name: {name}\nDiet: {diet}\nlocation: {location}\nType: {animal_type}\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
