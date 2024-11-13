7043d52529fe41e9ecfc25e87582900ead9b5eb6
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2024-11-13 19:46:25 +0100

    bring animal data into html

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 import json
 import os
 
+
 def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
+
+with open("animals_template.html", "r") as fileobj:
+    x = fileobj.read()
+
+
 animals_data = load_data('animals_data.json')
 
-
+output = ""
 for item in animals_data:
     name = item.get("name")
-    characteristics = item.get("characteristics", {})
     location = item["locations"][0]
+    characteristics = item.get("characteristics", {})
     diet = characteristics.get("diet")
     animal_type = characteristics.get("type", "N/A")
-
     if animal_type == "N/A":
-        print(f"Name: {name}\nDiet: {diet}\nlocation: {location}\n")
+        output += f"Name: {name}\n"
+        output += f"Location: {location}\n"
+        output += f"Diet: {diet}\n\n"
     else:
-        print(f"Name: {name}\nDiet: {diet}\nlocation: {location}\nType: {animal_type}\n")
+        output += f"Name: {name}\n"
+        output += f"Location: {location}\n"
+        output += f"Diet: {diet}\n"
+        output += f"Type: {animal_type}\n\n"
 
+s = x.replace("__REPLACE_ANIMALS_INFO__", output)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+with open("animals.html", "w") as file_obj:
+    file_obj.write(s)
