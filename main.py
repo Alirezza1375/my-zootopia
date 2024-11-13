9f7cfd5e958df898346726cda8c41082628f0f3d
Alirezza1375 <alirezagoudarzi1375@gmail.com>
2024-11-13 22:00:33 +0100

    more fields added to animal cards

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ with open("animals_template.html", "r") as fileobj:
 
 animals_data = load_data('animals_data.json')
 
+
+
 output = ""
 for item in animals_data:
     name = item.get("name")
@@ -19,12 +21,16 @@ for item in animals_data:
     characteristics = item.get("characteristics", {})
     diet = characteristics.get("diet")
     animal_type = characteristics.get("type", "N/A")
+    skin_type = characteristics.get("skin_type")
+    lifespan = characteristics.get("lifespan")
     if animal_type == "N/A":
         output += '<li class="cards__item">\n'
         output += f'<div class="card__title">{name}</div>\n'
         output += '<p class="card__text">\n'
         output += f"<strong>Location:</strong> {location}<br>\n"
         output += f"<strong>Diet:</strong> {diet}<br>\n"
+        output += f"<strong>Skin_type:</strong> {skin_type}<br>\n"
+        output += f"<strong>Lifespan:</strong> {lifespan}<br>\n"
         output += '</p>\n'
         output += '</li>\n'
     else:
@@ -34,10 +40,14 @@ for item in animals_data:
         output += f"<strong>Location:</strong> {location}<br>\n"
         output += f"<strong>Diet:</strong> {diet}<br>\n"
         output += f"<strong>Type:</strong> {animal_type}<br>\n"
+        output += f"<strong>Skin_type:</strong> {skin_type}<br>\n"
+        output += f"<strong>Lifespan:</strong> {lifespan}<br>\n"
         output += '</p>\n'
         output += '</li>\n'
 
+
 s = x.replace("__REPLACE_ANIMALS_INFO__", output)
+
 
 with open("animals.html", "w") as file_obj:
     file_obj.write(s)
